f19f416dd952881288104dc6d88a27a48eab579b
heejune <heejune1209@gmail.com>
2025-03-01 23:52:18 +0900

    deleet

--- Source/UE5_CPPver_Editor/UE5_CPPver_EditorModule.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Modules/ModuleManager.h"
-#include "../UE5_CPPver/R1Actor.h"
-
-//IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_CPPver, "UE5_CPPver" );
-IMPLEMENT_MODULE(FDefaultModuleImpl, UE5_CPPver_Editor);
